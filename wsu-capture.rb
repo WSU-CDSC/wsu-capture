d6e484d9f2354f9981f4c8e9298714eb95f30ae8
Andrew Weaver <weevz@uw.edu>
2019-03-27 16:15:19 -0700

    fix tab/space

--- wsu-capture.rb
+++ wsu-capture.rb
@@ -22,10 +22,10 @@ def recordVideo()
   outputFileName = $window.gets("Please Enter Output Name")
   outputFile = @outputDir + outputFileName + '.mkv'
   if File.exist?(outputFile)
-  	$window.alert("A file with that name already exists!")
+    $window.alert("A file with that name already exists!")
   else
-  	system('.\\ffmpeg -f dshow -pixel_format yuyv422 -i video="Osprey-827e MFI Video Device 1":audio="Unbal Input 1 (Osprey-827e 1)" -color_primaries smpte170m -color_trc bt709 -colorspace smpte170m -c:a pcm_s24le -c:v ffv1 -level 3 -g 1 -slices 16 -slicecrc 1 -vf setsar=40/27,setdar=4/3,setfield=bff,fieldorder=bff -y ' + '"' + outputFile + '"' + ' -f nut -vf setsar=40/27,setdar=4/3 - | .\\ffplay -')
-	end
+    system('.\\ffmpeg -f dshow -pixel_format yuyv422 -i video="Osprey-827e MFI Video Device 1":audio="Unbal Input 1 (Osprey-827e 1)" -color_primaries smpte170m -color_trc bt709 -colorspace smpte170m -c:a pcm_s24le -c:v ffv1 -level 3 -g 1 -slices 16 -slicecrc 1 -vf setsar=40/27,setdar=4/3,setfield=bff,fieldorder=bff -y ' + '"' + outputFile + '"' + ' -f nut -vf setsar=40/27,setdar=4/3 - | .\\ffplay -')
+  end
 end
 
 def editSettings()
