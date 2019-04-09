0f673756a4c4b716e60a48fe2beb1620dc22d8ad
Weaver <weevz@uw.edu>
2019-04-09 13:14:07 -0700

    start variables for device
    ffmpeg info

--- wsu-capture.rb
+++ wsu-capture.rb
@@ -12,7 +12,7 @@ def getOutputDir()
 end
 
 def previewVideo()
-  system(Ffmpeg_path + 'ffplay -f dshow -pixel_format yuv420p -i video="Osprey-827e MFI Video Device 1":audio="Unbal Input 1 (Osprey-827e 1)" -vf setsar=40/27,setdar=4/3,split=2[a][b],[b]format=pix_fmts=yuv420p,waveform=intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots,scale=720x480[bb],[a][bb]hstack')
+  system(Ffmpeg_path + 'ffplay -f dshow -pixel_format yuv420p -i video="' + Video_input + ':audio="' + Audio_input + '" -vf setsar=40/27,setdar=4/3,split=2[a][b],[b]format=pix_fmts=yuv420p,waveform=intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots,scale=720x480[bb],[a][bb]hstack')
 end
 
 def recordVideo()
@@ -26,7 +26,7 @@ def recordVideo()
   if File.exist?(outputFile)
     $window.alert("A file with that name already exists!")
   else
-    system(Ffmpeg_path +  'ffmpeg -f dshow -pixel_format yuv420p -i video="Osprey-827e MFI Video Device 1":audio="Unbal Input 1 (Osprey-827e 1)" -color_primaries smpte170m -color_trc bt709 -colorspace smpte170m -c:a pcm_s24le -c:v ffv1 -level 3 -g 1 -slices 16 -slicecrc 1 -vf setsar=40/27,setdar=4/3,setfield=bff,fieldorder=bff -y ' + '"' + outputFile + '"' + ' -f nut -vf setsar=40/27,setdar=4/3 -async 1 -vsync 1 - | ' + Ffmpeg_path + 'ffplay -')
+    system(Ffmpeg_path +  'ffmpeg -f dshow -pixel_format yuv420p -i video="' + Video_input + ':audio="' + Audio_input + '" -color_primaries smpte170m -color_trc bt709 -colorspace smpte170m -c:a pcm_s24le -c:v ffv1 -level 3 -g 1 -slices 16 -slicecrc 1 -vf setsar=40/27,setdar=4/3,setfield=bff,fieldorder=bff -y ' + '"' + outputFile + '"' + ' -f nut -vf setsar=40/27,setdar=4/3 -async 1 -vsync 1 - | ' + Ffmpeg_path + 'ffplay -')
     $window.alert("Making Derivatives")
     system(Ffmpeg_path + 'ffmpeg -i ' + '"' + outputFile + '"' + ' -c:v libx264 -c:a aac -movflags +faststart -crf 18 -b:a 128k -preset fast -vf "yadif,format=yuv420p" ' + derivativeFile)
     $window.alert("Derivatives Finished!")
@@ -34,7 +34,7 @@ def recordVideo()
 end
 
 def editSettings()
-  system(Ffmpeg_path + 'ffmpeg -show_video_device_dialog true -f dshow -i video="Osprey-827e MFI Video Device 1" -t 0.1 -f null -')
+  system(Ffmpeg_path + 'ffmpeg -show_video_device_dialog true -f dshow -i video="' + Video_input + '" -t 0.1 -f null -')
 end
 
 def openDocs()
